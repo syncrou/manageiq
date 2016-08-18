1593a362e020a115a2988652e1f435ce9408c5b4
Drew Bomhof <dbomhof@redhat.com>
2016-08-18 17:24:15 -0400

    Enabled Scheduled Automate tasks show view
    https://www.pivotaltracker.com/story/show/126163191

--- app/helpers/ops_helper.rb
+++ app/helpers/ops_helper.rb
@@ -1,6 +1,24 @@
 module OpsHelper
   include_concern 'TextualSummary'
 
+  def automate_humanized_filter(filter)
+    text = ''
+    filter.each do |key, val|
+      next if val.empty?
+      text << "#{key.to_s.capitalize.humanize}:</br> #{humanized_nested_keys(val)}</br>"
+    end
+    text.html_safe
+  end
+
+  def humanized_nested_keys(value)
+    text = ''
+    value.each do |key, val|
+      next if val.empty?
+      text << "&nbsp;&nbsp;#{key.to_s.humanize}: #{val}</br>"
+    end
+    text.html_safe
+  end
+
   def hide_button(button, opt)
     if opt == "on"
       button ? '' : 'display:none'
